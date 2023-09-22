79445ebac5365c302e4eebb8015c9816c91bdb0d
Mohanrajmohanms7 <145020971+Mohanrajmohanms7@users.noreply.github.com>
2023-09-22 00:43:04 -0700

    Add files via upload

---
+++ allinone.java
@@ -0,0 +1,45 @@
+ abstract class  solution               /* In this class I use 6 concepts 
+                                           this key,super key,abstract  
+                                            overloading,override,inheritance  */
+{
+    void display( int x,int y )
+    {    
+        // int x=80;
+       // int y=40;
+         System.out.println(x+y); 
+
+          System.out.println("void display");
+    }
+    void display1()
+    {
+        this.display(80,40);                              //thiskey
+         System.out.println("abstract class base class");
+        
+    }
+   abstract void display2();                                  // abstract
+}
+
+class solution1 extends solution                              // inheritance
+{
+    void display2()
+    {        
+            super.display1();                                 // super key
+             
+           System.out.println("derived class");
+           this.display2(20);                              // this key
+    }
+
+    void display2(int age)
+    {
+              System.out.println(age);
+    }
+}
+
+class allinone
+{
+    public static void main(String [] arg)
+    {
+         solution1 obj=new solution1();
+         obj.display2();
+    }
+}

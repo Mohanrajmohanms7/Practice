a85bcd454091d81ac7cf31068cf476a28c58a794
Mohanrajmohanms7 <145020971+Mohanrajmohanms7@users.noreply.github.com>
2023-09-26 01:01:24 -0700

    Add files via upload

---
+++ student.java
@@ -0,0 +1,16 @@
+package mohan;
+
+public class student
+
+{
+
+   public student(String name, String city,int age)
+   {
+        
+        System.out.println("student name="+name);
+        System.out.println("student age="+age);
+        System.out.println("student city="+city);
+   }  
+}
+
+

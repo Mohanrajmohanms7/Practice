371bf3dc8abb4018c0f84bfe303bf0756365ca3f
Mohanrajmohanms7 <145020971+Mohanrajmohanms7@users.noreply.github.com>
2023-09-26 01:03:14 -0700

    Add files via upload

---
+++ ball.java
@@ -0,0 +1,22 @@
+import mohan.student;
+
+ 
+class teacher extends student
+
+{
+     teacher(String name,String city,int age)
+     {
+        super(name,city,age);
+     }
+}
+  
+
+
+
+public class ball {
+    public static void main(String args[])
+    {
+        teacher o=new teacher("S", "salem", 19);
+         
+    }
+}

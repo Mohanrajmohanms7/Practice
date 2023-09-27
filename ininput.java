6384cb2a352121d2615aca859489d1af99a7ab51
Mohanrajmohanms7 <145020971+Mohanrajmohanms7@users.noreply.github.com>
2023-09-27 03:23:13 -0700

    Add files via upload

---
+++ ininput.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class ininput
+{
+ public static void main(String ard[])
+ {
+    Scanner in=new Scanner(System.in);
+    System.out.println("enter your father name");
+    String name=in.next();
+     System.out.println(" your father name="+name);
+
+     System.out.println("enter your father age");
+     int age=in.nextInt();
+      System.out.println(" your s father age="+age);
+
+
+ }
+
+}

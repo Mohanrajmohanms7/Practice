75a649dfd193e34797eee1306b1d46f548f27a24
Mohanrajmohanms7 <145020971+Mohanrajmohanms7@users.noreply.github.com>
2023-09-28 23:42:13 -0700

    Add files via upload

---
+++ demohan.java
@@ -0,0 +1,20 @@
+import mohan.bala;
+
+class mohan1 extends bala
+{
+  mohan1()
+  {
+     super.getname();     
+  }
+}
+
+
+
+class demohan 
+{
+    public static void main(String [] arg) 
+    {
+        mohan1 obj=new mohan1();
+        
+    }
+}

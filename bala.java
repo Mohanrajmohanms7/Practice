ef1ae9d5b0f55dc7c22ecdb27c66768e7dbcb001
Mohanrajmohanms7 <145020971+Mohanrajmohanms7@users.noreply.github.com>
2023-09-28 23:43:14 -0700

    Add files via upload

---
+++ bala.java
@@ -0,0 +1,18 @@
+package mohan;
+
+  public class bala
+{
+   public String name="mohanraj";
+
+  public bala()
+    {
+        System.out.println("class bala");
+    }
+
+    public void getname()
+    {
+          System.out.println("base class method");
+    }
+
+    
+}
---
+++ hrithik.java
@@ -0,0 +1,22 @@
+package mohan;
+
+class kumar extends bala
+{
+ kumar()
+ {
+    display();
+ }
+    void display()
+    {
+        System.out.println(name);
+    }
+}
+
+public class hrithik {
+    public static void main(String ard[])
+    {
+        kumar obj=new kumar();
+         obj.getname();
+    }
+    
+}

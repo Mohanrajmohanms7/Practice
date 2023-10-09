80b42a64fb88912109b0340c77a8b49e01a78d86
Mohanrajmohanms7 <145020971+Mohanrajmohanms7@users.noreply.github.com>
2023-10-08 23:19:32 -0700

    Add files via upload

---
+++ uplow.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class uplow
+{
+  public static void main(String [] awe) 
+  {
+     Scanner in =new Scanner(System.in);
+     System.out.println("enter you name =");
+     String name=in.nextLine();
+     System.out.println( name.toUpperCase());
+       System.out.println( name.toLowerCase());
+      
+  } 
+}

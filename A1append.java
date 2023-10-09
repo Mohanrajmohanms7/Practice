b1e150864f360804edc702d12e5163a6a4e08c70
Mohanrajmohanms7 <145020971+Mohanrajmohanms7@users.noreply.github.com>
2023-10-08 22:48:02 -0700

    Add files via upload

---
+++ A1append.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+public class A1append
+{
+   public static void main(String [] q)
+   {
+      StringBuilder bi=new StringBuilder(1);
+      bi.append("roll num  "+5) ; 
+      bi.append("name"+"mohanraj  ");
+      bi.append('M');
+      bi.append("percantage  "+22.2);
+      bi.append("cgp  "+1.11);
+      System.out.println(bi);
+
+   } 
+}

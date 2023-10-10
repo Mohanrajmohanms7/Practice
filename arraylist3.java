20b48024dd8ea2a79a13209f027b62fd2c31fb68
Mohanrajmohanms7 <145020971+Mohanrajmohanms7@users.noreply.github.com>
2023-10-09 23:54:46 -0700

    Add files via upload

---
+++ arraylist3.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class arraylist3
+ {
+     public static void main(String msd[])
+  {
+    ArrayList li=new ArrayList();
+    li.add("mohanraj");
+    li.add("hari");
+    li.add("praveen");
+    li.add("suraj");
+    li.add("dinesh");
+    li.add("saravanan");
+
+
+
+    li.remove(0);
+    System.out.println(li);
+  }   
+}

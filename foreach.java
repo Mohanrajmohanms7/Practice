ab0abe10cca6023c92d80caef95fbe09d1d9d568
Mohanrajmohanms7 <145020971+Mohanrajmohanms7@users.noreply.github.com>
2023-10-11 22:55:35 -0700

    Add files via upload

---
+++ foreach.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class foreach {
+    public static void main(String [] ms)
+    {
+        StringBuilder  ex=new StringBuilder();
+        
+        ex.append("tekfilo,");
+        ex.append("innovation,");
+        ex.append("private,");
+        ex.append("limit");
+        
+        System.out.println(ex);
+
+        
+    }
+}

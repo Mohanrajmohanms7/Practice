86d55e72286f8b91c1011bc1a6e786a5abf46831
Mohanrajmohanms7 <145020971+Mohanrajmohanms7@users.noreply.github.com>
2023-10-11 23:46:46 -0700

    Add files via upload

---
+++ ADouble.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+public class ADouble
+{
+   public static void main(String []a)
+   { 
+
+       Scanner in=new Scanner(System.in);
+       System.out.println("enter percentage=");
+       double m=in.nextDouble();
+
+
+       System.out.println("atteance percentage="+m+"%");
+
+   } 
+}
+
+

8316bea877122dea6871c3cb8eeb07cc73394a6f
Mohanrajmohanms7 <145020971+Mohanrajmohanms7@users.noreply.github.com>
2023-10-13 02:09:37 -0700

    Add files via upload

---
+++ mathcos.java
@@ -0,0 +1,11 @@
+public class mathcos    // math cos
+{
+    public static void main(String [] ms)
+    {
+     System.out.println("sin="+Math.cos(1));
+     System.out.println("sin="+Math.cos(2));
+     System.out.println("sin="+Math.cos(3));
+     System.out.println("sin="+Math.cos(4));
+     System.out.println("sin="+Math.cos(5));
+    }
+}

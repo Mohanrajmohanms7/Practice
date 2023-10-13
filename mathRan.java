ad35c97c29aa7b19ec843df3b404b173d44f31bb
Mohanrajmohanms7 <145020971+Mohanrajmohanms7@users.noreply.github.com>
2023-10-13 02:08:22 -0700

    Add files via upload

---
+++ mathRan.java
@@ -0,0 +1,14 @@
+public class mathRan      // math random
+{
+      public static void main(String [] Ard)
+      {
+        
+
+        System.out.println("random number="+Math.random());
+        System.out.println("random number="+Math.random());
+        System.out.println("random number="+Math.random());
+        System.out.println("random number="+Math.random());
+        System.out.println("random number="+Math.random());
+
+      }
+}

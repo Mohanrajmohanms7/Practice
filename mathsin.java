eec1a1e516f4f705c58a9eea89b4b41e81fd1b8a
Mohanrajmohanms7 <145020971+Mohanrajmohanms7@users.noreply.github.com>
2023-10-13 02:10:38 -0700

    Add files via upload

---
+++ mathsin.java
@@ -0,0 +1,11 @@
+public class mathsin               // math sin
+{   
+    public static void main(String [] ms)
+    {
+        System.out.println("sin="+Math.sin(1));
+         System.out.println("sin="+Math.sin(2));
+          System.out.println("sin="+Math.sin(3));
+           System.out.println("sin="+Math.sin(4));
+            System.out.println("sin="+Math.sin(5));
+    }   
+}

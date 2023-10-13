a404fc47fe89fa7eb6ffd073d11dcfae1920213e
Mohanrajmohanms7 <145020971+Mohanrajmohanms7@users.noreply.github.com>
2023-10-12 23:56:35 -0700

    Add files via upload

---
+++ math1.java
@@ -0,0 +1,12 @@
+public class math1                               // Math minimun
+{
+    public static void main(String []ms)
+    {
+        
+        int mark1=95;
+        int mark2=85;
+
+        System.out.println(".....math function....");
+        System.out.print("min mark="+Math.min(mark1, mark2));
+    }
+}

1b243a1521f64e85593cd1b79b438e44dee3cd3f
Mohanrajmohanms7 <145020971+Mohanrajmohanms7@users.noreply.github.com>
2023-10-13 03:05:46 -0700

    Add files via upload

---
+++ mathlog.java
@@ -0,0 +1,28 @@
+public class mathlog    // Math log
+{
+   public static void main(String []msd)
+   {
+       int value =1;
+       double num=2;
+       float num1=3;
+         
+       System.out.println("value calculation");
+       System.out.println(Math.log(value));
+       System.out.println(Math.log10(value));
+       System.out.println(Math.log1p(value));
+
+        System.out.println("num calculation");
+       System.out.println(Math.log(num));
+       System.out.println(Math.log10(num));
+       System.out.println(Math.log1p(num));
+       
+         System.out.println("num1 calculation");
+       System.out.println(Math.log(num1));
+       System.out.println(Math.log10(num1));
+       System.out.println(Math.log1p(num1));
+
+
+
+      
+   } 
+}

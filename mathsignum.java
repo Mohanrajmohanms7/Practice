953f8532ff24b1ff0697d6ca506c0a32ca165444
Mohanrajmohanms7 <145020971+Mohanrajmohanms7@users.noreply.github.com>
2023-10-13 06:45:21 -0700

    Add files via upload

---
+++ mathsignum.java
@@ -0,0 +1,20 @@
+class signum
+{
+    signum()
+    {
+        double a= 12221.0/00;
+        double b=2/1; 
+        System.out.println(Math.signum(a));
+         System.out.println(Math.signum(b));
+
+
+    }
+}
+
+public class mathsignum 
+{
+    public static void main(String [] msd)
+    {
+        signum ob=new signum();
+    }
+}

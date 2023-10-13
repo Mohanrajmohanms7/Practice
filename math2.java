5595df7a252fdbe3b1cd90a04d3edaa6f3c6a665
Mohanrajmohanms7 <145020971+Mohanrajmohanms7@users.noreply.github.com>
2023-10-12 23:56:54 -0700

    Add files via upload

---
+++ math2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class math2
+{
+     public static void main(String []ms)
+     {
+        Scanner in =new Scanner(System.in);
+
+        System.out.println("Enter tamil mark=");
+        int T=in.nextInt();
+
+         System.out.println("Enter english mark=");
+        int E=in.nextInt();
+
+        System.out.println("max mark in tamil and engalish="+Math.max(T, E));
+
+        
+        
+
+
+
+
+     }  
+}

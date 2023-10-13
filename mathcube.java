18c059d3d8717b9dc9270877979a457aa621586f
Mohanrajmohanms7 <145020971+Mohanrajmohanms7@users.noreply.github.com>
2023-10-13 03:07:11 -0700

    Add files via upload

---
+++ mathcube.java
@@ -0,0 +1,25 @@
+class cube     // Math cube
+{   
+    cube()
+    {
+      int a=8;
+      int b=27;
+
+        
+      System.out.println("cube="+Math.cbrt(a));
+      System.out.println("cube="+Math.cbrt(b));
+
+    }
+}
+
+
+
+
+
+public class mathcube       // math cube
+{
+     public static void main(String []ms)
+     {
+        cube on=new cube();
+     }
+}

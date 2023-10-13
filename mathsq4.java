b820ff36fe906f288fa67e6572719419fa744086
Mohanrajmohanms7 <145020971+Mohanrajmohanms7@users.noreply.github.com>
2023-10-13 01:33:34 -0700

    Add files via upload

---
+++ mathsq4.java
@@ -0,0 +1,15 @@
+public class mathsq4            // math absolute
+{
+   public static void main(String [] ms)
+   {
+       int value=-10;
+       double value1=-10.123;
+       float value2=-10;
+       short value3=-10;
+
+       System.out.println(Math.abs(value));
+       System.out.println(Math.abs(value1));
+       System.out.println(Math.abs(value2));
+       System.out.println(Math.abs(value3));
+   }
+}

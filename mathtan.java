34dab4c9d678af9f91b5bcd3c1688b346697ee7d
Mohanrajmohanms7 <145020971+Mohanrajmohanms7@users.noreply.github.com>
2023-10-13 02:11:24 -0700

    Add files via upload

---
+++ mathtan.java
@@ -0,0 +1,11 @@
+public class mathtan      // Math  tan
+{
+   public static void main(String [] w) 
+   {
+    System.out.println("sin="+Math.cos(10));
+    System.out.println("sin="+Math.cos(20));
+    System.out.println("sin="+Math.cos(30));
+    System.out.println("sin="+Math.cos(40));
+    System.out.println("sin="+Math.cos(50));
+   } 
+}

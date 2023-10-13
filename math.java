a2e966762b6b335a814ad5e593660ffc07fea9e3
Mohanrajmohanms7 <145020971+Mohanrajmohanms7@users.noreply.github.com>
2023-10-12 23:56:14 -0700

    Add files via upload

---
+++ math.java
@@ -0,0 +1,12 @@
+import java.lang.Math;
+public class math                                  // Math maxmimum
+{
+     public static void main(String [] ard)
+     {
+          int mark1=95;
+          int mark2=85;
+
+          System.out.println(".....math function....");
+          System.out.print("max mark="+Math.max(mark1, mark2));
+     }
+}

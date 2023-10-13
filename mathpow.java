28d28db91f93daadda384ee26e0aecf9def7a71a
Mohanrajmohanms7 <145020971+Mohanrajmohanms7@users.noreply.github.com>
2023-10-13 03:06:13 -0700

    Add files via upload

---
+++ mathpow.java
@@ -0,0 +1,22 @@
+class pow
+{
+    pow()
+    {
+        System.out.println(Math.pow( 1,2));
+        System.out.println(Math.pow(1,3 ));
+        System.out.println(Math.pow( 2,1));
+        System.out.println(Math.pow( 2,2));
+        System.out.println(Math.pow( 4,4));
+        System.out.println(Math.pow( 5,5));
+    }
+}
+
+
+
+public class mathpow 
+{
+    public static void main(String msd[])
+    {
+       pow m=new pow();
+    }
+}

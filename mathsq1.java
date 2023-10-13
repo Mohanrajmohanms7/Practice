e46a0c9fba215e173c00471e66ebabec3b1dd03b
Mohanrajmohanms7 <145020971+Mohanrajmohanms7@users.noreply.github.com>
2023-10-13 01:32:28 -0700

    Add files via upload

---
+++ mathsq1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+import java.lang.Math;                 // math sqrt
+
+ class mathsq1 
+{
+     public static void main(string []ard)
+     {
+         Scanner in=new Scanner(System.in);
+
+        System.out.println("Enter the number ");
+        int value=in.nextInt();
+
+        System.out.println("Square root of the number="+Math.sqrt(value));
+
+
+     }
+}
